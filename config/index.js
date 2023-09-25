44914b4f3be78b5952b379440a2972b7dd0e578d
qixinsheji <146009379+qixinsheji@users.noreply.github.com>
2023-09-25 23:43:02 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,10 +1,10 @@
 module.exports = {
-  appid: "wx71941f6106ae1072",
-  appsecret: "b4b2ad8a8d4a94e403831886ac10c646",
-  template_id: "q14lTqsDyK0N-KcZUs8UKHOl3GWn2uErDk-8hbm2ouQ",
-  touser: "oonWJ5slWuS2OrJdgCLM3GmNdxVk",
+  appid: "wxadc78b5f038ca14d",
+  appsecret: "a82f798aaa9298f507e6ef38fb54ed36",
+  template_id: "1AxB3WskPxDnlxXN2g-jP1arQhWj_RsqPwJEPKxgFOY",
+  touser: "oG1C76AfNeiS6z0c0GukvIp_K0m0",
 
   key: "201c93c2e2b34bdeecdbf9f5265eb84e",
-  city: "邢台",
+  city: "温州",
   first_date: "2022-11-10",
 };
